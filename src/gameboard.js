feea17102b7ec95e6649546d2843ed8b7505860d
Ian Hart <ianhart13@gmail.com>
2022-06-02 22:59:56 -0400

    fixed gameboard parse error & added expect board empty test

--- src/gameboard.js
+++ src/gameboard.js
@@ -9,12 +9,18 @@ function buildBoard() {
         const boardColumns = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
         let num = Math.floor(i / 10);
         let column = i + 1 - num * 10;
-        column = column.toString();
 
         this.squareArr.push(boardColumns[num] + column);
       }
       console.log(this.squareArr);
       return this.squareArr;
+    },
+    isEmpty() {
+      if (this.squareArr.includes('x' || 'o')) {
+        return false;
+      } else {
+        return true;
+      }
     }
   };
 }
@@ -22,24 +28,3 @@ function buildBoard() {
 module.exports = {
   buildBoard
 };
-
-/*
-class Board {
-  constructor() {
-    this.columns = 10;
-    this.rows = 10;
-  }
-  buildDataSets() {
-    //i is equivaliant to columns and e is rows
-    for (i = 1; e < 11; i++) {
-      if (i === 10) {
-        e++;
-        i = 0;
-      }
-      const div = document.createElement('div');
-      div.dataset['box'];
-      div.dataset.box = `${i}, ${e}`;
-    }
-  }
-}
-*/
--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -1,16 +1,23 @@
 const boardModule = require('./gameboard');
-const buildBoard = boardModule.buildBoard;
+//const buildBoard = boardModule.buildBoard;
+
+beforeEach(() => {
+  return (buildBoard = boardModule.buildBoard());
+});
 
 test('Expect the gameboard to have 10 columns', () => {
-  expect(buildBoard().columns).toBe(10);
+  expect(buildBoard.columns).toBe(10);
 });
 
 test('Expect the gameboard to have 10 rows', () => {
-  expect(buildBoard().rows).toBe(10);
+  expect(buildBoard.rows).toBe(10);
 });
 
 test('Expect board squares to equal 100', () => {
-  let board = buildBoard();
-  board.displayBoard();
+  let board = buildBoard.displayBoard();
   expect(board.squareArr.length).toBe(100);
+});
+
+test('Expect new board to be empty', () => {
+  expect(buildBoard.isEmpty()).toBe(true);
 });
