fe42e9536cef7a6e3ee44ef16b812160e3eacadd
Ian Hart <ianhart13@gmail.com>
2022-08-31 22:31:52 -0400

    fixed classList.add error on shipDivs func

--- src/index.js
+++ src/index.js
@@ -64,7 +64,7 @@ function setBoard(user) {
   const playerBoard = document.querySelector('#playerBoard');
   const compBoard = document.querySelector('#compBody');
 
-  console.log('this is the gameboard length' + user.gameBoard.squareArr.length);
+  //console.log('this is the gameboard length' + user.gameBoard.squareArr.length);
   // places ships onto board via drag and drop for player1
   if (user.name === 'Computer') {
     for (let i = 0; i < user.gameBoard.squareArr.length; i++) {
@@ -102,7 +102,7 @@ function shipDivs(ship, container, user, shipName) {
   const childContainer = document.createElement('div');
   const div = document.createElement('div');
   div.className = 'ship-container';
-  div.classList.add(user.ships.name);
+  div.classList.add(ship.name);
   for (let i = 0; i < ship.length; i++) {
     const shipPart = document.createElement('div');
     shipPart.className = 'ship';
