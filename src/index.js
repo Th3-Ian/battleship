ff527fc95ce187ea6af265ff2c6a544af8014928
Ian Hart <ianhart13@gmail.com>
2022-09-26 12:06:45 -0400

    removed endGame func

--- src/index.js
+++ src/index.js
@@ -87,10 +87,6 @@ function makeAttack() {
   }
 }
 
-function endGame() {
-  modalModule.openModal('Game Win', 'Would you like to start a new game?');
-}
-
 function buildFleet() {
   let fleet = [];
   for (let i = 0; i < shipTypesArr.length; i++) {
